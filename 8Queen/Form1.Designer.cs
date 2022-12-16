72f7b16765d3b8b6ff4deb390b7d8cae5f62026e
Artin <azomorodian@yahoo.com>
2022-12-16 16:34:06 +0330

    Fix the problem and make it simple code

---
+++ 8Queen/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace _8Queen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Show = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(659, 12);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(78, 27);
+            this.start.TabIndex = 0;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(646, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(213, 589);
+            this.listBox1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 600);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Show
+            // 
+            this.Show.Location = new System.Drawing.Point(755, 12);
+            this.Show.Name = "Show";
+            this.Show.Size = new System.Drawing.Size(78, 27);
+            this.Show.TabIndex = 3;
+            this.Show.Text = "Show";
+            this.Show.UseVisualStyleBackColor = true;
+            this.Show.Click += new System.EventHandler(this.Show_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 667);
+            this.Controls.Add(this.Show);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "8Queen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Show;
+    }
+}
+
---
+++ 8Queen/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _8Queen
+{
+    public partial class Form1 : Form
+    {
+        private int[] chess = new int[8];
+        private Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                chess[0] = i;
+                queens(0);
+                for (int f = 0; f < 8; f++)
+                    chess[f] = 0;
+            }            
+        }
+
+        private void queens(int index)
+        {
+            if (check(index))
+                if (index == 7)
+                {
+                    string str = string.Empty;
+                    for (int i = 0; i < 8; i++)
+                        str += chess[i] + "-";                    
+                    listBox1.Items.Add(str);
+                        
+                }
+                else
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        chess[index + 1] = j;
+                        queens(index + 1);
+
+                    }
+                }
+
+        }
+
+        private bool check(int index)
+        {
+            int k=0;
+            bool switchs=true;
+
+            while(k<index && switchs)
+            {
+                if (chess[index]==chess[k] || Math.Abs(chess[index]-chess[k])==index-k)
+                    switchs=false;
+                k++;
+            }
+            return switchs;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 8; i++)
+                chess[i] = 0;
+
+            #region chess Ground
+
+            drawChessGround();
+            
+            #endregion
+        }
+
+        private void Show_Click(object sender, EventArgs e)
+        {
+            string select = string.Empty;
+            int x = 0;
+            int y = 0;
+
+            select = listBox1.SelectedItem.ToString();
+            drawChessGround();
+            for (int i=0;i<8;i++)
+            {
+                x = Convert.ToInt32(select.Substring(0,1))*74;
+                y = i*74;
+                select = select.Remove(0, 2);
+                g.DrawImage(_8Queen.Properties.Resources.vc, x, y, 74, 74);               
+            }                       
+        }
+
+        private void drawChessGround()
+        {
+            int x = 0;
+            int y = 0;
+            int color = 1;
+
+            this.BackColor = Color.LightGray;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (color % 2 == 0)
+                        g.FillRectangle(Brushes.Black, x, y, 74, 74);
+                    else
+                        g.FillRectangle(Brushes.White, x, y, 74, 74);
+                    x += 74;
+                    color++;
+                }
+                y += 74;
+                x = 0;
+                color++;
+            }
+        }
+    }
+}
